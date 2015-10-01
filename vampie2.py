8d8ac7c6829209552e45c5da8329f4401728fa71
DJMcClellan1966 <DJMcClellan1966@gmail.com>
2015-10-01 07:03:54 -0400

    Create vampie2.py

---
+++ vampie2.py
@@ -0,0 +1,9 @@
+if name == 'Alice':
+  print("Hi, Alice.")
+elif age < 12:
+  print("you are not Alice, kiddo!")
+elif age > 100:
+  print("you are not Alice, granny!")
+elif age > 1000
+  print("unlike you, Alice is not an undead, immortal vampire?!")
+  
